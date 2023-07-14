088a496cc0987db90de4fd2bc4fe65afb88035d2
Kerem PEKARLI <kerempekarli@gmail.com>
2023-07-14 21:49:49 +0300

    Font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
 
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Nunito", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
